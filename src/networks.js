4c3ee8053e435437c65e9c8355329ef0b5dffdf6
segwit <segwitcoin@gmail.com>
2017-09-08 12:59:08 +0300

    Add net parameters

--- src/networks.js
+++ src/networks.js
@@ -3,14 +3,14 @@
 
 module.exports = {
   bitcoin: {
-    messagePrefix: '\x18Bitcoin Signed Message:\n',
+    messagePrefix: '\x18ATBCoin Signed Message:\n',
     bech32: 'bc',
     bip32: {
       public: 0x0488b21e,
       private: 0x0488ade4
     },
-    pubKeyHash: 0x00,
-    scriptHash: 0x05,
+    pubKeyHash: 0x17,
+    scriptHash: 0x53,
     wif: 0x80
   },
   testnet: {
@@ -23,15 +23,5 @@ module.exports = {
     pubKeyHash: 0x6f,
     scriptHash: 0xc4,
     wif: 0xef
-  },
-  litecoin: {
-    messagePrefix: '\x19Litecoin Signed Message:\n',
-    bip32: {
-      public: 0x019da462,
-      private: 0x019d9cfe
-    },
-    pubKeyHash: 0x30,
-    scriptHash: 0x32,
-    wif: 0xb0
   }
 }
